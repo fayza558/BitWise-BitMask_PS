2b099988ef32c3cf460bb03dab5c119f905be74c
fayza mahmoud <110182701+fayza558@users.noreply.github.com>
2025-02-20 20:09:45 +0200

    Create F - Raising Bacteria.cpp

---
+++ F - Raising Bacteria.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+#define ll long long
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<__builtin_popcount(n)<<endl;
+    return 0;
+}
+
